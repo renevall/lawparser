aa3812a498abfdae8254cc32eea3be1732b71046
Rene Vallecillo <reneval@gmail.com>
2015-07-01 17:02:23 -0600

    fixing bug where odd articles where not processed.

--- parser/ptext.go
+++ parser/ptext.go
@@ -1,7 +1,7 @@
 package parser
 
 import (
-	"fmt"
+	// "fmt"
 	"io/ioutil"
 	"log"
 	"regexp"
@@ -55,32 +55,29 @@ func FindTags(data []string) (map[int]string, []int) {
 	}
 
 	sort.Ints(keys)
-	fmt.Println(keys)
 	return m, keys
 
 }
 
 func MakeLaw(data []string, index []int, ref map[int]string) Titles {
 	var title_index, chapter_index, article_index int
-	var is_article bool = false
 	article_txt := []string{}
 	var titles = Titles{}
 
 	title_index, chapter_index, article_index = -1, -1, -1
+	last := index[len(index)-1]
 
 	for r, k := range index {
 		if ref[k] == "Titulo" {
 			title_index = title_index + 1
 			chapter_index = -1
 			article_index = -1
-			is_article = false
 			titles = append(titles, Title{Name: data[k]})
 		}
 
 		if ref[k] == "Capitulo" {
 			chapter_index = chapter_index + 1
 			article_index = -1
-			is_article = false
 
 			titles[title_index].Chapters =
 				append(titles[title_index].Chapters, Chapter{Name: data[k]})
@@ -89,26 +86,24 @@ func MakeLaw(data []string, index []int, ref map[int]string) Titles {
 
 		if ref[k] == "Arto" {
 
-			if !is_article {
-				article_index = article_index + 1
+			article_index = article_index + 1
 
-				is_article = true
-				//fmt.Println("procesando linea: ", k)
-				titles[title_index].Chapters[chapter_index].Articles =
-					append(titles[title_index].Chapters[chapter_index].Articles, Article{Name: data[k-1]})
+			// fmt.Println("procesando linea: ", k)
+			titles[title_index].Chapters[chapter_index].Articles =
+				append(titles[title_index].Chapters[chapter_index].Articles, Article{Name: data[k-1]})
+			if k != last {
 				for x := k; x < index[r+1]-1; x += 1 {
-					if is_article == true {
-						article_txt = append(article_txt, data[x])
-					}
+					article_txt = append(article_txt, data[x])
 				}
 			} else {
-				is_article = false
-				//fmt.Println("Article Index: ", article_index)
-				titles[title_index].Chapters[chapter_index].Articles[article_index].Text =
-					strings.Join(article_txt, " ")
-				article_txt = nil
-				is_article = false
+				for x := k; x <= len(data)-1; x += 1 {
+					article_txt = append(article_txt, data[x])
+				}
 			}
+
+			titles[title_index].Chapters[chapter_index].Articles[article_index].Text =
+				strings.Join(article_txt, " ")
+			article_txt = nil
 
 		}
 
