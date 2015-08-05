82285eded748ae7045eb4fd5d0a06b571d6b1cad
Rene Vallecillo <reneval@gmail.com>
2015-08-05 14:59:09 -0600

    web server now working with app and parser

--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"bitbucket.com/reneval/lawparser/api"
+	"bitbucket.com/reneval/lawparser/db"
+	// "bitbucket.com/reneval/lawparser/parser"
+	_ "github.com/mattn/go-sqlite3"
+	"golang.org/x/net/context"
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	ctx := context.Background()
+	ctx = db.OpenSQL(ctx, "main", "sqlite3", "root:hunter2@unix(/tmp/mysql.sock)/myCoolDB")
+	defer db.Close(ctx) // closes all DB connections
 
 	router := api.NewRouter()
 
