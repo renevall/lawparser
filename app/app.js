5f606029213a3cc92d044d8851d451ed7b25dbed
Rene Vallecillo <reneval@gmail.com>
2016-03-14 21:07:06 -0600

    deleting old angular front end

--- app/app.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('myapp',["ngResource"]);
-
-app.controller("MainCtl", [ "$scope", "$resource", function($scope, $resource){
-
-	var Law = $resource("/Law/:id", {id: "@id"}, {});
-
-	$scope.books = Law.query();
-})
