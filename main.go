66d1be2b8fd11d5c7543402df4149b57d8fa89c2
Rene Vallecillo <reneval@gmail.com>
2016-05-03 11:45:22 -0600

    serving the frontend with syslink to keep repos separated

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	// "bitbucket.com/reneval/lawparser/api"
+
+	"net/http"
+
 	"bitbucket.org/reneval/lawparser/db"
 	"bitbucket.org/reneval/lawparser/handlers"
 	// "bitbucket.org/reneval/lawparser/parser"
@@ -19,13 +22,14 @@ func main() {
 		log.Fatalln("Could not connect to database")
 	}
 
-	// router := api.NewRouter()
-
 	r := httprouter.New()
-	r.GET("/", handlers.Index)
-	r.POST("/upload", handlers.FileUpload(db))
-	r.GET("/articles", handlers.GetAllArticles(db))
-	n := negroni.Classic()
+	r.POST("/api/upload", handlers.FileUpload(db))
+	r.GET("/api/articles", handlers.GetAllArticles(db))
+	n := negroni.New(
+		negroni.NewRecovery(),
+		negroni.NewLogger(),
+		negroni.NewStatic(http.Dir("app")),
+	)
 	n.UseHandler(r)
 	n.Run(":8080")
 }
