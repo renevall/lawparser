ddb978b598fee6d6968ccdb16d05ade5aa39b6c1
Rene Vallecillo <reneval@gmail.com>
2016-06-13 23:45:59 -0600

    improved parser

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"sort"
 	"sync"
 
 	"bitbucket.org/reneval/lawparser/models"
@@ -39,24 +40,57 @@ func StreamLines(uri string) <-chan string {
 	return out
 }
 
-func FindCTags(in <-chan string, wg *sync.WaitGroup) {
+func FindCTags(in <-chan string, wg *sync.WaitGroup) (chan map[int]string, chan []int) {
 	wg.Add(1)
-	go func() {
-		defer wg.Done()
+
+	chm := make(chan map[int]string)
+	chkeys := make(chan []int)
+
+	go func(chan map[int]string, chan []int) {
+
+		var keys []int
+		t := make(map[int]string)
+
+		i := 0
+
 		for text := range in {
+			fmt.Println(text)
 			for _, tag := range tags {
 				r, _ := regexp.Compile(tag.regex)
 				if r.MatchString(text) {
+					t[i+1] = tag.name
+					break
 				}
 			}
+			i++
 		}
-	}()
+
+		chm <- t
+
+		for k, v := range t {
+			keys = append(keys, k)
+			if v == "Titulo" {
+				title = title + 1
+			}
+		}
+
+		sort.Ints(keys)
+		chkeys <- keys
+
+		close(chm)
+		close(chkeys)
+		wg.Done()
+
+	}(chm, chkeys)
+
+	return chm, chkeys
 
 }
 
-func FindBasicData(in <-chan string, wg *sync.WaitGroup) *models.Law {
+func FindBasicData(done chan<- struct{}, in <-chan string, wg *sync.WaitGroup) *models.Law {
 	fmt.Println("find basic data")
 	law := new(models.Law)
+
 	wg.Add(1)
 	go func(*models.Law) {
 		defer wg.Done()
@@ -64,31 +98,26 @@ func FindBasicData(in <-chan string, wg *sync.WaitGroup) *models.Law {
 			for _, tag := range intro {
 				r, _ := regexp.Compile(tag.regex)
 				if r.MatchString(text) {
-					fmt.Println("match!")
-					fmt.Println(tag.name)
 					switch tag.name {
 					case "Name":
-						fmt.Println(text)
 						fillBasicData(tag.name, text, law)
 						break
 
 					case "Number":
-						fmt.Println(text)
 						fillBasicData(tag.name, text, law)
 						break
 
 					case "Aproved":
-						fmt.Println(text)
 						fillBasicData(tag.name, text, law)
 						break
 
 					case "Diary":
-						fmt.Println(text)
 						fillBasicData(tag.name, text, law)
 						break
 
 					case "Arto":
 						fmt.Println("End of Law Header Reached")
+						done <- struct{}{}
 						wg.Done()
 						return
 					}
@@ -121,11 +150,16 @@ func fillBasicData(tag string, value string, law *models.Law) {
 func ParseConcurrent(uri string) *models.Law {
 	wg := new(sync.WaitGroup)
 
+	done := make(chan struct{})
+
 	fmt.Println("Parse concurrent")
 	in := StreamLines(uri)
-	chs := fanOut(in, wg)
-	law := FindBasicData(chs[0], wg)
-	FindCTags(chs[1], wg)
+	chs := fanOut(done, in, wg)
+
+	law := FindBasicData(done, chs[0], wg)
+	tags, _ := FindCTags(chs[1], wg)
+
+	fmt.Println("Tags: ", <-tags)
 
 	wg.Wait()
 
@@ -133,16 +167,29 @@ func ParseConcurrent(uri string) *models.Law {
 
 }
 
-func fanOut(ch <-chan string, wg *sync.WaitGroup) []chan string {
+func fanOut(done <-chan struct{}, ch <-chan string, wg *sync.WaitGroup) []chan string {
 
 	cs := []chan string{
 		make(chan string),
 		make(chan string),
 	}
+
+	var closed bool = false
 	go func() {
 		for i := range ch {
-			for _, w := range cs {
-				w <- i
+			for r, w := range cs {
+				select {
+				case <-done:
+					closed = true
+					break
+
+				default:
+					if r == 0 && !closed {
+						w <- i
+					} else if r != 0 {
+						w <- i
+					}
+				}
 			}
 		}
 		for _, c := range cs {
